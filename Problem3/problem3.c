2b9ec1f38cc53113bdfa2c130f16687bb4864527
Ikram Ul Haq <44068298+ikramulhaqch@users.noreply.github.com>
2019-03-01 12:47:21 +0500

    Update Program3.c

--- Problem3/problem3.c
+++ Problem3/problem3.c
@@ -12,15 +12,46 @@
 **	This function take file pointer as paramter to read content from and 
 **	char pointer as an second argument which points to string to search in file
 */
-void mygrep(FILE*, char*);
+void mygrep(FILE* file, char* ch){
+  while(!feof(file))
+{
+	char temp[200];
+fgets(temp,1000,file);
+if(strstr(temp,ch))
+  printf("%s",temp);
+}
+}
 
 /*	
 **	This function take file pointer as paramter to read content from and 
 **	char pointer as an second argument which points to string to search in file
 ** 	and char pointer as an third argument to replace the string finded with it.
 */
-void myreplace(FILE *fp,char *find, char * replace);
 
+
+
+
+
+  void myreplace(FILE *fp,char *ch, char * replace)
+{
+
+int i, j=0, k;
+  for(i=0; file[i]; i++)
+  {
+    if(file[i] == ch[j])
+    {
+      int x=i;
+      for(k=i, j=0; file[k] && ch[j]; j++, k++)
+        if(file[k]!=ch[j])
+            break;
+       
+       for(int l=0,m=i; file[m] && ch[l]; m++,l++)
+        file[m]=replace[l];
+    }
+    }
+  }
+
+}
 
 int  main(int argc,char *argv[])
 {
@@ -29,32 +60,33 @@ int  main(int argc,char *argv[])
 	/*	creating variables	
 */
 
-	int behaviour;
+	char behaviour;
 	FILE *fp;
 	char *filename=argv[1];
 	char *find=argv[2];
 	char * replace;
 
 	/*	check if mygrep is called or myreplace	
-*/
-	if(/*	check if the name of executable is mygrep	*/ )
+*/	
+	if(argv[0]=="mygrep" )
 	{
+		printf("ikram is ok\n");
 		if(argc != 3)
 		{
 			printf("usage\t./mygrep filename <string-to-search>\n");
 			exit(1);
 		}
 
-		behaviour = GREP;
+		behaviour = 'G';
 	}
-	else if(/*	check if the name of executable is myreplace	*/)
+	else if(argv[0]=="myreplace")
 	{
 		if(argc != 4)
 		{
 			printf("\t./myreplace filename  <string-to-search> <string-to-replace>");
 			exit(1);
 		}
-		behaviour = REPLACE;
+		behaviour = 'R';
 		replace = argv[3];
 	}
 	else
@@ -69,57 +101,19 @@ int  main(int argc,char *argv[])
 
 	fp=fopen(filename,"rt");
 
-	if(behaviour == GREP)
+	if(behaviour == 'G')
 	{
 		mygrep(fp,find);		/*	caling function	
 */
 	}
-	else if ( behaviour == REPLACE )
+	else if ( behaviour == 'R' )
 	{
 		myreplace(fp,find,replace);		/*	calling myreplace	
-*/
+
 	}
-	
-	fclose(fp);		/*	closing file	
-*/
+*/	
+	fclose(fp);		/*	closing file	*/
+
+
 	return 0;
 }
-
-
-void mygrep(FILE *fp,char *find)
-{
-	char c1[500];
-
-	/*	Add code to get strings from file
-*/ 
-	while(/*	read a string from file*/)
-	{
-		/*	Add your code here to search a string find on string c1 readed from file	*/
-	}
-}
-
-
-
-
-
-void myreplace(FILE *fp,char *find, char * replace)
-{
-	char c1[500];
-	int flen = strlen(find);
-
-
-	while(/*	read a string from file*/)
-	{
-		/*	Add your code here to search a string find on string c1 readed from file	
-*/
-		if(/*	found the string 	*/)
-		{
-			/*	replace the finded string with replace string	*/
-		}
-	}
-
-
-
-}
-
-
